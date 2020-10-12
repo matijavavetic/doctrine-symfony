389d3d6e06b01074ecc6fe9689304f2032876a78
Matija <matija.vavetic@gmail.com>
2020-10-12 09:52:02 +0200

    Doctrine persist

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +21,37 @@ class QuestionController extends AbstractController
         $this->isDebug = $isDebug;
     }
 
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $em)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1,100))));
+
+    }
+        $em->persist($question);
+        $em->flush();
+
+        return new Response(sprintf(
+            'Well hello! This shiny new question is id #%d, slug %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
 
     /**
      * @Route("/", name="app_homepage")
