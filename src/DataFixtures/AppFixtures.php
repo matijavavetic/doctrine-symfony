d7a501af5eeba4ac2c96b0ca4f6c91dbd48b2277
Matija <matija.vavetic@gmail.com>
2020-10-13 19:04:56 +0200

    Refactor

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Question;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,6 +10,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        QuestionFactory::new()->create();
+        QuestionFactory::new()->createMany(15);
     }
 }
